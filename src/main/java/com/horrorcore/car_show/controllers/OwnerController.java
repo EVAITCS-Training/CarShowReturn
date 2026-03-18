d2d50ebf01e5777de3354cf36fd55607008d62d0
horrorcoresoftware <matthew-martin@programmer.net>
2026-03-18 10:50:24 -0400

    Spring Rest API OwnerController.java Changed

--- src/main/java/com/horrorcore/car_show/controllers/OwnerController.java
+++ src/main/java/com/horrorcore/car_show/controllers/OwnerController.java
@@ -1,57 +1,41 @@
 package com.horrorcore.car_show.controllers;
 
 import com.horrorcore.car_show.dtos.OwnerRequest;
-import com.horrorcore.car_show.enums.Gender;
+import com.horrorcore.car_show.dtos.OwnerResponse;
+import com.horrorcore.car_show.services.CarService;
 import com.horrorcore.car_show.services.OwnerService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("/owner")
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/owner")
 @RequiredArgsConstructor
 public class OwnerController {
     private final OwnerService ownerService;
 
     @GetMapping(value = {"/", ""})
-    public String ownerIndex(Model model){
-        model.addAttribute("listOfOwners", ownerService.getAllOwners());
-        return "owner/index";
-    }
-
-    @GetMapping(value = {"/create", "/create/"})
-    public String ownerForm(Model model){
-        model.addAttribute(
-                "newOwner", new OwnerRequest(
-                        0,
-                        "",
-                        "",
-                        null,
-                        null));
-        model.addAttribute("genderOptions", Gender.values());
-        return "owner/form";
+    public ResponseEntity<List<OwnerResponse>> ownerIndex(){
+        return ResponseEntity.ok(ownerService.getAllOwners());
     }
 
     @PostMapping(value = {"/create", "/create/"})
-    public String createOwner(@Valid OwnerRequest ownerRequest, BindingResult bindingResult, Model model){
-        if (bindingResult.hasErrors()) {
-            model.addAttribute("newOwner", ownerRequest);
-            model.addAttribute("genderOptions", Gender.values());
-            return "owner/form";
-        }
-        ownerService.createOwner(ownerRequest);
-        return "redirect:/owner/";
+    public ResponseEntity<OwnerResponse> createOwner(@Valid @RequestBody OwnerRequest ownerRequest){
+        return ResponseEntity.created(null).body(ownerService.createOwner(ownerRequest));
     }
 
-    @PostMapping("/delete/{id}")
-    public String deleteOwner(@PathVariable long id){
-        ownerService.deleteOwnerById(id);
-        return "redirect:/owner/";
+    @PutMapping(value = {"{id}/update"})
+    public ResponseEntity<OwnerResponse> updateOwnerRequest(@Valid @RequestBody OwnerRequest ownerRequest) {
+        return ResponseEntity.created(null).body(ownerService.createOwner(ownerRequest));
     }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteOwner(@PathVariable long id){
+        ownerService.deleteOwnerById(id);
+        return ResponseEntity.ok().build();
+    }
+
 }
